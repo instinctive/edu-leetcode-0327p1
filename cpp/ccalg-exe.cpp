e85e6ac12e9939ecb86d9252c2818e062d3a488c
instinctive <177694+instinctive@users.noreply.github.com>
2023-01-20 10:22:47 -0800

    Add cases for alternate algs

--- cpp/ccalg-exe.cpp
+++ cpp/ccalg-exe.cpp
@@ -1,9 +1,22 @@
+#include <cstring>
 #include <iostream>
 #include <string>
 
 #include "ccalg.h"
 
 int main(int argc, char **argv) {
-    int x = readAndCount(argv[1]);
+    auto usage = [argv]() {
+        fprintf(stderr, "Usage: %s [eqrange|obvious] input-file", argv[0]);
+        exit(1);
+    };
+    if (argc != 3) usage();
+    char* impl = argv[1];
+    char* path = argv[2];
+    int x;
+    if (!strcmp(impl, "eqrange")) {
+        x = ext_eqrange(path);
+    } else if (!strcmp(impl, "obvious")) {
+        x = ext_obvious(path);
+    } else usage();
     std::cout << argv[1] << ": " << x << std::endl;
 }
